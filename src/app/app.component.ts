87915f0a7947451b7c24ee75d5eddc777089a3de
MEHALA KELLER <mk0853251@otc.edu>
2019-11-18 14:48:24 -0600

    adding items

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: '<div><h1>{{ title }}</h1></div>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'This is pretty cool';
+}
